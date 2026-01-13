0836b8c58ab33ce0f74e508432f5049ff96eff2a
SujitDevl <pandasujit687@gmail.com>
2026-01-13 18:02:56 +0530

    initialize cafe grind app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-100">
+      <h1 className="text-4xl font-bold text-gray-800">Cafe Grind ☕</h1>
+    </div>
+  );
+}
+
+export default App;
